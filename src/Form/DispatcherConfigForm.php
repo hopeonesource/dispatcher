4da65c47c98d4ffcedfa0239565aa1c46615fbb5
michael mwebaze <michael.mwebaze@gmail.com>
2018-11-14 11:17:27 -0500

    added support for ajax call back to config form

--- src/Form/DispatcherConfigForm.php
+++ src/Form/DispatcherConfigForm.php
@@ -1,10 +1,13 @@
 <?php
 namespace Drupal\dispatcher\Form;
 
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\HtmlCommand;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 
 class DispatcherConfigForm extends ConfigFormBase {
+    private $plugin_manager;
 
     /**
      * {@inheritdoc}
@@ -27,17 +30,17 @@ class DispatcherConfigForm extends ConfigFormBase {
      */
     public function buildForm(array $form, FormStateInterface $form_state) {
         $config = $this->config('dispatcher.config');
-        $plugin_manager = \Drupal::service('plugin.manager.dispatcher');
-        $plugin_definitions = $plugin_manager->getDefinitions();
+        $this->plugin_manager = \Drupal::service('plugin.manager.dispatcher');
+        $plugin_definitions = $this->plugin_manager->getDefinitions();
 
         $contentType = $config->get('content_type');
         $messageField = $config->get('message_field');
 
         if (isset($contentType)){
-            $instance = $plugin_manager->createInstance($contentType);
+            $instance = $this->plugin_manager->createInstance($contentType);
         }
         else{
-            $instance = $plugin_manager->createInstance(array_values($plugin_definitions)[0]['id']);
+            $instance = $this->plugin_manager->createInstance(array_values($plugin_definitions)[0]['id']);
         }
 
         $fields = [];
@@ -47,11 +50,16 @@ class DispatcherConfigForm extends ConfigFormBase {
             $fields = $instance->getContentTypeFields();
         }
 
+        $form['#attached']['library'][] = 'dispatcher/dispatcher_settings';
         $form['content_type'] = array (
             '#title' => t('Content type'),
             '#type' => 'select',
             '#options' => $content_type_options,
             '#default_value' => isset($contentType) ? $contentType : '',
+            '#ajax' => array(
+                'callback' => '::getFields',
+                'event' => 'change',
+            ),
             '#required' => TRUE,
         );
         $form['message_field'] = array(
@@ -59,6 +67,9 @@ class DispatcherConfigForm extends ConfigFormBase {
             '#type' => 'radios',
             '#options' => $fields,
             '#default_value' => isset($messageField) ? $messageField : '',
+            '#required' => TRUE,
+            '#prefix' => '<div id="message-fields">',
+            '#suffix' => '</div>',
         );
 
         return parent::buildForm($form, $form_state);
@@ -78,7 +89,35 @@ class DispatcherConfigForm extends ConfigFormBase {
         $this->config('dispatcher.config')
             ->set('content_type', $form_state->getValue('content_type'))
             ->set('message_field', $form_state->getValue('message_field'))
-            //->set('508_message', $form_state->getValue('508_message'))
             ->save();
+    }
+    public function getFields(array &$form, FormStateInterface $form_state){
+        $config = $this->config('dispatcher.config');
+        $contentType = $config->get('content_type');
+        $msgField = $config->get('message_field');
+
+        $selectedContentType = $form_state->getValue('content_type');
+
+        $ajax_response = new AjaxResponse();
+        $instance = $this->plugin_manager->createInstance($selectedContentType);
+        $fields = $instance->getContentTypeFields();
+
+        $msg = '';
+        foreach ($fields as $field){
+            if ($field == $msgField && $contentType == $selectedContentType){
+                $msg .= "<div class=\"js-form-item form-item js-form-type-radio form-type-radio js-form-item-message-field form-item-message-field\">";
+                $msg .= "<input checked=\"checked\" type=radio name=message_field class=\"form-radio\" required=required  value=".$field."><label for=\"edit-message-field-title\" class=\"option\" >".$field."</label>";
+                $msg .= "</div>";
+            }
+            else{
+                $msg .= "<div class=\"js-form-item form-item js-form-type-radio form-type-radio js-form-item-message-field form-item-message-field\">";
+                $msg .= "<input type=radio name=message_field class=\"form-radio\" required=required value=".$field."><label for=\"edit-message-field-title\" class=option>".$field."</label>";
+                $msg .= "</div>";
+            }
+        }
+
+        $ajax_response->addCommand(new HtmlCommand('.fieldset-wrapper', $msg));
+
+        return $ajax_response;
     }
 }
