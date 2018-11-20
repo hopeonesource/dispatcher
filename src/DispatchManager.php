175f2cf551edb0665c40f042b2c759f7ddda8465
Ryan Aubrey <rma7qb@virginia.edu>
2018-11-20 15:15:28 -0500

    Adding more detailed exception message in logging for queueMessage

--- src/DispatchManager.php
+++ src/DispatchManager.php
@@ -57,9 +57,11 @@ class DispatchManager implements DispatchManagerInterface {
         try {
             $this->smsProvider->queue($message);
         } catch (RecipientRouteException $e) {
-            \Drupal::logger('dispatch manager')->warning('RecipientRouteException thrown in DispatchManager->sendMessage');
+            \Drupal::logger('dispatch manager')->warning('RecipientRouteException thrown in DispatchManager->queueMessage.\n\n' . serialize($e));
         } catch (SmsDirectionException $e) {
-            \Drupal::logger('dispatch manager')->warning('SmsDirection Exception thrown in DispatchManager->sendMessage');            
+            \Drupal::logger('dispatch manager')->warning('SmsDirection Exception thrown in DispatchManager->queueMessage.\n\n' . serialize($e));
+        } catch (SmsException $e) {
+            \Drupal::logger('dispatch manager')->warning('SmsException thrown in DispatchManager->queueMessage.\n\n' . serialize($e));
         }
     }
 
